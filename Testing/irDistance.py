6dcbf9f39ac3fee38b8963f59bc6f3f77b2cc123
swordsounds <126785544+swordsounds@users.noreply.github.com>
2023-12-04 15:14:26 -0330

    yeah

--- Testing/irDistance.py
+++ Testing/irDistance.py
@@ -2,30 +2,33 @@ from Phidget22.Phidget import *
 from Phidget22.Devices.VoltageRatioInput import *
 import time
 
-def onSensorChange(self, sensorValue, sensorUnit):
-	print("SensorValue: " + str(sensorValue))
-	print("SensorUnit: " + str(sensorUnit.symbol))
-	print("----------")
 
 voltageRatioInput0 = VoltageRatioInput()
 voltageRatioInput1 = VoltageRatioInput()
 
+
+def onSensorChange(self, sensorValue, sensorUnit):
+	print("SensorValue [" + str(self.getChannel()) + "]: " + str(sensorValue))
+	print("SensorUnit [" + str(self.getChannel()) + "]: " + str(sensorUnit.symbol))
+	print("----------")
+
 def main():
-	voltageRatioInput0.setOnSensorChangeHandler(onSensorChange)
-	voltageRatioInput1.setOnSensorChangeHandler(onSensorChange)
+	
 
 	voltageRatioInput0.setChannel(0)
 	voltageRatioInput1.setChannel(1)
-	
+
+	voltageRatioInput0.setOnSensorChangeHandler(onSensorChange)
+	voltageRatioInput1.setOnSensorChangeHandler(onSensorChange)
+
 	voltageRatioInput0.openWaitForAttachment(5000)
 	voltageRatioInput1.openWaitForAttachment(5000)
 
 	voltageRatioInput0.setSensorType(VoltageRatioSensorType.SENSOR_TYPE_1101_SHARP_2Y0A21)
 	voltageRatioInput1.setSensorType(VoltageRatioSensorType.SENSOR_TYPE_1101_SHARP_2Y0A21)
 
-
 def close():
 	voltageRatioInput0.close()
+	voltageRatioInput1.close()
 
-if __name__ == "__main__":
-	main()
+main()
